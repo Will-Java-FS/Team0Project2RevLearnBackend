f7b1271ec1e97064f4350a4a5f7b1bad377fb8da
RyanTHSTGS <ryschwaghy@gmail.com>
2024-08-26 13:01:35 -0700

    Spring Initializer

---
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/LessonPlanSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.LessonPlanSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonPlanSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonPlanSysApplication.class, args);
+	}
+
+}
---
+++ LessonPlanSys/src/test/java/com/example/LessonPlanSys/LessonPlanSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LessonPlanSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonPlanSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
