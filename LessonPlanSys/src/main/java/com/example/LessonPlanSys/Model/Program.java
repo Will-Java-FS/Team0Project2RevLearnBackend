a5621bd6737b2e4bde374c6723c39180a4be5423
Ruar Podushkin <rpodushkin@gmail.com>
2024-08-26 17:06:07 -0400

    Add basic structure for Program

---
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Model/Program.java
@@ -0,0 +1,30 @@
+package com.example.LessonPlanSys.Model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name="programs")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Program {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="program_id", updatable = false)
+    private int id;
+
+    @Column(nullable = false)
+    private String programName;
+
+    //TODO: Uncomment when Course model is done.
+/*    @OneToMany(mappedBy = "program", fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<Course> courses;*/
+
+}
---
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Service/ProgramServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.LessonPlanSys.Service;
+
+import com.example.LessonPlanSys.Model.Program;
+import com.example.LessonPlanSys.Repo.ProgramRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ProgramServiceImpl implements ProgramService {
+    @Autowired
+    ProgramRepo pr;
+
+    @Override
+    public Program getProgram(int id) {
+        return pr.findById(id).orElse(null);
+    }
+
+    @Override
+    public Program addProgram(Program program) {
+        return pr.save(program);
+    }
+
+    @Override
+    public Program updateProgram(int id, Program newProgram) {
+        Program existingProgram = pr.findById(id).orElse(null);
+
+        if (existingProgram == null) {
+            return null;
+        }
+
+        //TODO: Adjust so that the courses are also copied over
+        existingProgram.setProgramName(newProgram.getProgramName());
+        //existingProgram.setProgramCourses(newProgram.getProgramCourses());
+
+        return pr.save(existingProgram);
+
+    }
+
+    @Override
+    public void deleteProgram(int id) {
+        pr.deleteById(id);
+    }
+
+    @Override
+    public List<Program> getAllPrograms() {
+        return pr.findAll();
+    }
+}
