8835473e00f6de94733d3c1a278985417eea2b2b
beckerst707 <beckerst@sonoma.edu>
2024-08-27 12:29:47 -0700

    updated enrollments model

--- LessonPlanSys/src/main/java/com/example/LessonPlanSys/Model/Enrollments.java
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Model/Enrollments.java
@@ -16,10 +16,12 @@ public class Enrollments {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(mappedBy = "enrollments", cascade = CascadeType.ALL)
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private List<User> users;
 
-    @ManyToOne(mappedBy = "enrollments", cascade = CascadeType.ALL)
+    @ManyToOne
+    @JoinColumn(name = "course_id")
     private List<Course> courses;
 
     @Column(nullable = false, length = 255)
