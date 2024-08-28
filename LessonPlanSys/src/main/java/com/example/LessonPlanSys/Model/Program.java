520f076d248005ab837b38aa8b2e624fee3e85f7
Ruar Podushkin <rpodushkin@gmail.com>
2024-08-28 16:51:43 -0400

    Running version

--- LessonPlanSys/src/main/java/com/example/LessonPlanSys/Model/Program.java
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Model/Program.java
@@ -2,7 +2,6 @@ package com.example.LessonPlanSys.Model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.*;
 
 import java.util.List;
