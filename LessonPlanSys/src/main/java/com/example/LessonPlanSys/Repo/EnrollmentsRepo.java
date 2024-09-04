632acd240f7c2d576c75926b2d007ea5125eeef7
RyanTHSTGS <ryschwaghy@gmail.com>
2024-09-04 13:02:56 -0700

    Updated query to include unlinked lesson plans

--- LessonPlanSys/src/main/java/com/example/LessonPlanSys/Repo/EnrollmentsRepo.java
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Repo/EnrollmentsRepo.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface EnrollmentsRepo extends JpaRepository<Enrollments, Integer> {
-    @Query(value = "SELECT uls.complete FROM user_lesson_status AS uls LEFT JOIN lessonplan_course ON lessonplan_course.lesson_plan_id = uls.lesson_plan_id WHERE uls.user_id = ?1 AND lessonplan_course.course_id = ?2", nativeQuery = true)
+    @Query(value = "SELECT COALESCE(uls.complete, false) FROM user_lesson_status AS uls Right JOIN lessonplan_course ON lessonplan_course.lesson_plan_id = uls.lesson_plan_id WHERE COALESCE(uls.user_id,1) = ?1 AND lessonplan_course.course_id = ?2", nativeQuery = true)
     List<Boolean> getAllULS(int uid, int cid);
 }
