f2107b74c4344db1b79b9d27e6bcc5d58216c5c7
Andrew T <me@andrewtran-shi.dev>
2024-09-05 02:25:45 -0700

    modified:   LessonPlanSys/src/main/resources/data.sql 	modified:   LessonPlanSys/src/main/resources/schema.sql

--- LessonPlanSys/src/main/resources/schema.sql
+++ LessonPlanSys/src/main/resources/schema.sql
@@ -20,13 +20,13 @@ CREATE SEQUENCE IF NOT EXISTS lesson_course_id_seq;
 -- Drop tables if they exist
 DROP TABLE IF EXISTS user_lesson_status CASCADE;
 
-DROP TABLE IF EXISTS lesson_courses CASCADE;
-
 DROP TABLE IF EXISTS lesson_plan_course CASCADE;
 
-DROP TABLE IF EXISTS lesson_plan CASCADE;
+DROP TABLE IF EXISTS lesson_courses CASCADE;
 
 DROP TABLE IF EXISTS lesson_plans CASCADE;
+
+DROP TABLE IF EXISTS lesson_plan CASCADE;
 
 DROP TABLE IF EXISTS forum_posts CASCADE;
 
@@ -41,14 +41,14 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS programs CASCADE;
 
 -- Create tables
-CREATE TABLE
-    IF NOT EXISTS programs (
+CREATE TABLE IF NOT EXISTS
+    programs (
         program_id SERIAL PRIMARY KEY,
         program_name VARCHAR NOT NULL
     );
 
-CREATE TABLE
-    IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS
+    users (
         user_id SERIAL PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         username VARCHAR(50) NOT NULL UNIQUE,
@@ -61,8 +61,8 @@ CREATE TABLE
         program_id INT REFERENCES programs (program_id)
     );
 
-CREATE TABLE
-    IF NOT EXISTS courses (
+CREATE TABLE IF NOT EXISTS
+    courses (
         course_id SERIAL PRIMARY KEY,
         course_name VARCHAR NOT NULL,
         created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -74,8 +74,8 @@ CREATE TABLE
         CONSTRAINT fk_teacher FOREIGN KEY (teacher_id) REFERENCES users (user_id)
     );
 
-CREATE TABLE
-    IF NOT EXISTS discussionforums (
+CREATE TABLE IF NOT EXISTS
+    discussionforums (
         forum_id INTEGER NOT NULL DEFAULT nextval ('discussionforums_id_seq') PRIMARY KEY,
         course_id INTEGER NOT NULL,
         title VARCHAR NOT NULL,
@@ -84,8 +84,8 @@ CREATE TABLE
         CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES courses (course_id)
     );
 
-CREATE TABLE
-    IF NOT EXISTS enrollments (
+CREATE TABLE IF NOT EXISTS
+    enrollments (
         enroll_id SERIAL PRIMARY KEY,
         enrollment_status VARCHAR(255) NOT NULL,
         payment_status VARCHAR(255) NOT NULL,
@@ -95,8 +95,8 @@ CREATE TABLE
         CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (user_id)
     );
 
-CREATE TABLE
-    IF NOT EXISTS forum_posts (
+CREATE TABLE IF NOT EXISTS
+    forum_posts (
         forum_post_id SERIAL PRIMARY KEY,
         created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -107,8 +107,8 @@ CREATE TABLE
         CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (user_id)
     );
 
-CREATE TABLE
-    IF NOT EXISTS lesson_plans (
+CREATE TABLE IF NOT EXISTS
+    lesson_plans (
         lesson_plan_id SERIAL PRIMARY KEY,
         content TEXT NOT NULL,
         created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -116,13 +116,8 @@ CREATE TABLE
         title VARCHAR NOT NULL
     );
 
-<<<<<<< HEAD
-=======
-CREATE UNIQUE INDEX IF NOT EXISTS lesson_plan_pkey ON lesson_plans (lesson_plan_id);
-
->>>>>>> 7f8f54475e504848ace76c02eaa7113b6011097c
-CREATE TABLE
-    IF NOT EXISTS lesson_plan_course (
+CREATE TABLE IF NOT EXISTS
+    lesson_plan_course (
         lesson_course_id INTEGER NOT NULL DEFAULT nextval ('lesson_course_id_seq') PRIMARY KEY,
         lesson_plan_id INTEGER NOT NULL,
         course_id INTEGER NOT NULL,
@@ -130,13 +125,8 @@ CREATE TABLE
         CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES courses (course_id)
     );
 
-<<<<<<< HEAD
-=======
-CREATE UNIQUE INDEX IF NOT EXISTS lesson_plan_course_pkey ON lesson_plan_course (lesson_course_id);
-
->>>>>>> 7f8f54475e504848ace76c02eaa7113b6011097c
-CREATE TABLE
-    IF NOT EXISTS user_lesson_status (
+CREATE TABLE IF NOT EXISTS
+    user_lesson_status (
         user_lesson_id SERIAL PRIMARY KEY,
         complete BOOLEAN NOT NULL,
         lesson_plan_id INTEGER,
