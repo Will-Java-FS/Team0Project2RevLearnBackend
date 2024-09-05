cc9e469bba545b1a263517ebb490fa5ca71f744e
Oscar Ramirez <oramirezent@gmail.com>
2024-09-05 14:57:50 -0400

    LessonPlan Controller CRUD Functional!

--- LessonPlanSys/src/main/java/com/example/LessonPlanSys/Service/LessonPlanServiceImp.java
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Service/LessonPlanServiceImp.java
@@ -1,7 +1,11 @@
 package com.example.LessonPlanSys.Service;
 
+import com.example.LessonPlanSys.Model.LessonCourse;
 import com.example.LessonPlanSys.Model.LessonPlan;
+import com.example.LessonPlanSys.Model.UserLessonStatus;
+import com.example.LessonPlanSys.Repo.LessonCourseRepo;
 import com.example.LessonPlanSys.Repo.LessonPlanRepo;
+import com.example.LessonPlanSys.Repo.UserLessonStatusRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +13,17 @@ import java.sql.Timestamp;
 import java.util.List;
 
 @Service
-public class LessonPlanServiceImp implements LessonPlanService{
+public class LessonPlanServiceImp implements LessonPlanService {
     LessonPlanRepo lessonPlanRepo;
+    LessonCourseRepo lessonCourseRepo;
+    UserLessonStatusRepo userLessonStatusRepo;
+
     @Autowired
-    public LessonPlanServiceImp(LessonPlanRepo lessonPlanRepo)
-    {
+    public LessonPlanServiceImp(LessonPlanRepo lessonPlanRepo, LessonCourseRepo lessonCourseRepo,
+                                UserLessonStatusRepo userLessonStatusRepo) {
         this.lessonPlanRepo = lessonPlanRepo;
+        this.lessonCourseRepo = lessonCourseRepo;
+        this.userLessonStatusRepo = userLessonStatusRepo;
     }
 
     @Override
@@ -35,8 +44,32 @@ public class LessonPlanServiceImp implements LessonPlanService{
     }
 
     @Override
-    public void deleteLessonPlan(int id) {
-         lessonPlanRepo.deleteById(id);
+    public void deleteLessonPlan(int lesson_plan_id) {
+        // exist ? continue: skip
+        LessonPlan exists = getById(lesson_plan_id);
+        if (exists == null) {
+            return;
+        }
+
+        // Remove lesson from courses
+        List<LessonCourse> lessonCourses = lessonCourseRepo.findAll()
+                .stream()
+                .filter(lc -> lc.getLessonPlan().getLesson_plan_id() == lesson_plan_id)
+                .toList();
+        lessonCourses.forEach(lc ->
+                lessonCourseRepo.deleteById(lc.getLesson_course_id())
+        );
+
+        // Remove lesson from user progress
+        List<UserLessonStatus> userLessonStatuses = userLessonStatusRepo.findAll()
+                .stream()
+                .filter(ul -> ul.getLessonPlan().getLesson_plan_id() == lesson_plan_id)
+                .toList();
+        userLessonStatuses.forEach(ul ->
+                userLessonStatusRepo.deleteById(ul.getUser_lesson_id()));
+
+        // Remove lesson
+        lessonPlanRepo.deleteById(lesson_plan_id);
     }
 
     @Override
