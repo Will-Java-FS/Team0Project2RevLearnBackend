3da615259853bb54cf62d4f700f4ca11f163f72b
Andrew Tran-Shi <me@andrewtran-shi.dev>
2024-09-10 17:29:35 -0700

    modified:   LessonPlanSys/pom.xml 	modified:   LessonPlanSys/src/main/java/com/example/LessonPlanSys/Authorize/AuthResponse.java 	modified:   LessonPlanSys/src/main/java/com/example/LessonPlanSys/Authorize/JwtService.java 	modified:   LessonPlanSys/target/classes/application.properties

--- LessonPlanSys/src/main/java/com/example/LessonPlanSys/Authorize/AuthResponse.java
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Authorize/AuthResponse.java
@@ -8,7 +8,6 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
-    
     private String token;
     private String username;
     private Integer userId;
--- LessonPlanSys/src/main/java/com/example/LessonPlanSys/Authorize/JwtService.java
+++ LessonPlanSys/src/main/java/com/example/LessonPlanSys/Authorize/JwtService.java
@@ -1,6 +1,7 @@
 package com.example.LessonPlanSys.Authorize;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,8 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class JwtService {
-    public static final String SECRET = "secret";
+    public static final String SECRET = Base64.getEncoder().encodeToString("YourSecretKeyMustBeLongEnough12345678901234567890".getBytes());
+
     private static final long validity = TimeUnit.MINUTES.toMillis(30);
 
     private SecretKey generateKey() {
@@ -19,33 +21,33 @@ public class JwtService {
     }
     
     public String generateToken(UserDetails userDetails) {
-        Map<String, String> claims = new HashMap<>();
+        Map<String, Object> claims = new HashMap<>();
         claims.put("this is a ?", "test");
 
         return Jwts.builder()
-            .claims(claims)
-            .subject(userDetails.getUsername())
-            .issuedAt(Date.from(Instant.now()))
-            .expiration(Date.from(Instant.now().plusMillis(validity)))
+            .addClaims(claims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(Date.from(Instant.now()))
+            .setExpiration(Date.from(Instant.now().plusMillis(validity)))
             .signWith(generateKey())
             .compact();
     }
 
     public String extractUsername(String token) {
-        Claims claims = Jwts.parser()
-            .verifyWith(generateKey())
+        Claims claims = Jwts.parserBuilder()
+            .setSigningKey(generateKey())
             .build()
-            .parseSignedClaims(token)
-            .getPayload();
+            .parseClaimsJws(token)
+            .getBody();
         return claims.getSubject();
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parser()
-            .verifyWith(generateKey())
+        return Jwts.parserBuilder()
+            .setSigningKey(generateKey())
             .build()
-            .parseSignedClaims(token)
-            .getPayload();
+            .parseClaimsJws(token)
+            .getBody();
     }
 
     public boolean isTokenValid(String token){
